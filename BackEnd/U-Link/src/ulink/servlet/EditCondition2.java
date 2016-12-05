e3c3eaa4f518674a89ae5b508899f211c727d0f2
seankwok-2014 <Seankwok.2014@smu.edu.sg>
2016-12-06 00:31:03 +0800

    updated latest copy

---
+++ BackEnd/U-Link/src/ulink/servlet/EditCondition2.java
@@ -0,0 +1,66 @@
+package ulink.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.reflect.TypeToken;
+
+import ulink.constructor.Condition;
+import ulink.dao.DatabaseConnection;
+
+/**
+ * Servlet implementation class EditCondition2
+ */
+@WebServlet("/EditCondition2")
+public class EditCondition2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditCondition2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		HttpSession session = request.getSession();
+		
+		String condition = (String) session.getAttribute("condition");
+		DatabaseConnection database = new DatabaseConnection();
+		Condition conditionType = database.retrieveConditionDetails(condition);	
+		PrintWriter out = response.getWriter();
+		Gson gson = new Gson();
+	
+		  String arrayListToJson = gson.toJson(conditionType);
+		  	System.out.println(arrayListToJson);
+			out.write(arrayListToJson);
+			out.flush();
+		return;
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+	}
+
+}
