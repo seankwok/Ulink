8d7e5a4008b5f297804adf086f9c1b9e40e1acd3
obbyx <Nic11111!>
2016-12-18 15:44:32 +0800

    replace integration folder

--- Integration/U-Link/src/ulink/servlet/UploadServlet.java
+++ Integration/U-Link/src/ulink/servlet/UploadServlet.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -29,6 +30,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import com.google.gson.JsonObject;
 
+@WebServlet("/UploadServlet")
 public class UploadServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
