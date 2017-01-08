479a4f17450a92cbf05439408e0194c2f4fdd1b6
obbyx <Nic11111!>
2017-01-08 16:38:14 +0800

    edit password

---
+++ Integration/U-Link/src/ulink/servlet/EditUserPassword2.java
@@ -0,0 +1,67 @@
+package ulink.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.reflect.TypeToken;
+
+import ulink.constructor.Condition;
+import ulink.dao.DatabaseConnection;
+
+/**
+ * Servlet implementation class EditCondition2
+ */
+@WebServlet("/EditUserPassword2")
+public class EditUserPassword2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditUserPassword2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		HttpSession session = request.getSession();
+		
+		String email =  (String) session.getAttribute("email");
+		String password =  (String) session.getAttribute("password");
+		DatabaseConnection database = new DatabaseConnection();
+		PrintWriter out = response.getWriter();
+	
+		String jsonInString = email;
+		System.out.print(jsonInString);
+		out.write(jsonInString);
+		out.flush();
+		
+	
+		return;
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+	}
+
+}
