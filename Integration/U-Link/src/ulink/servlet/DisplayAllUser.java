591324dcdfa703662eb32530d93fdef16c14aa59
seankwok-2014 <Seankwok.2014@smu.edu.sg>
2017-01-09 23:44:45 +0800

    protected

--- Integration/U-Link/src/ulink/servlet/DisplayAllUser.java
+++ Integration/U-Link/src/ulink/servlet/DisplayAllUser.java
@@ -11,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
@@ -26,48 +27,68 @@ import ulink.dao.DatabaseConnection;
 @WebServlet("/DisplayAllUser")
 public class DisplayAllUser extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DisplayAllUser() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.setContentType("application/json");
-		
-		PrintWriter out = response.getWriter();
-		DatabaseConnection connection = new DatabaseConnection();
-		ArrayList<User> userList = connection.getUser();
-		System.out.print(userList.size());
-		JsonArray result = (JsonArray) new Gson().toJsonTree(userList, new TypeToken<List<User>>() {}.getType());
-		
-		String json = new Gson().toJson(result);
-		/*
-		request.setAttribute("userList", userList);
-		RequestDispatcher rd=request.getRequestDispatcher("accountManagement.jsp");  
-		rd.forward(request, response);
-		*/
-
-		out.write(json);
-		out.flush();
-		return;
+	public DisplayAllUser() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("application/json");
+		HttpSession session = request.getSession();
+		String roles = (String) session.getAttribute("admin");
+
+		PrintWriter out = response.getWriter();
+		DatabaseConnection connection = new DatabaseConnection();
+
+		if (roles.equals("admin")) {
+			ArrayList<User> userList = connection.getUser();
+			System.out.print(userList.size());
+			JsonArray result = (JsonArray) new Gson().toJsonTree(userList, new TypeToken<List<User>>() {
+			}.getType());
+
+			String json = new Gson().toJson(result);
+			/*
+			 * request.setAttribute("userList", userList); RequestDispatcher
+			 * rd=request.getRequestDispatcher("accountManagement.jsp");
+			 * rd.forward(request, response);
+			 */
+
+			out.write(json);
+			out.flush();
+			return;
+		} else {
+			String json = "fail";
+			/*
+			 * request.setAttribute("userList", userList); RequestDispatcher
+			 * rd=request.getRequestDispatcher("accountManagement.jsp");
+			 * rd.forward(request, response);
+			 */
+
+			out.write(json);
+			out.flush();
+			return;
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-		
-		
+
 	}
 
 }
