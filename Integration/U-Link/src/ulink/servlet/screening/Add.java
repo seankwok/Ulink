a9423d3c30a90fb7a19729b4461d405f2a8917ed
seankwok-2014 <Seankwok.2014@smu.edu.sg>
2017-02-25 12:27:49 +0800

    updated screening

--- Integration/U-Link/src/ulink/servlet/screening/Add.java
+++ Integration/U-Link/src/ulink/servlet/screening/Add.java
@@ -50,8 +50,10 @@ public class Add extends HttpServlet {
 			boolean check = true;
 			for (int k = 0; k < conditionList.size(); k++) {
 				Condition condition = conditionList.get(k);
-			
-				if (condition.getConditionName().equals(illness) && condition.getType().equals(type[i])){
+				System.out.println( type[i] + " gender " + condition.getType());
+				System.out.println(condition.getConditionName() + " Condition  " + illness);
+				System.out.println(condition.getScreening() + " Screening " + screening);
+				if (condition.getConditionName().equals(illness) && condition.getType().equals(type[i]) && condition.getScreening().equals(screening)){
 			
 					check = false;
 				}
